7d2f2faa25085eb996ebc3307383e83bcff0128d
lynamst <stephen.lynam23@mail.dcu.ie>
2018-01-08 14:45:32 +0000

    New Project

---
+++ com/grapher/binancePull.py
@@ -0,0 +1,20 @@
+class binancePull():
+    
+    def print_list_length(self):
+        return len(self.lst)
+    
+    x = 32
+    
+    test = 'Hello, this is a new string'
+    
+    lst=[test]
+    
+    lst.append(", this is another list part")
+    print(len(lst))
+    print(*lst, sep='\n')
+    
+    print('x')
+    
+    length=print_list_length()
+    print(length)
+    
